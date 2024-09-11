ec19a148cf9f4bb2d12d984bd14b0d053e10becf
Mohamed Elzomor <mohamed.elzomor@outlook.com>
2024-09-12 00:20:41 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Form from "./components/Form/Form.tsx";
+import "./App.css";
+
+function App() {
+
+    return (
+        <Form></Form>
+    )
+}
+
+export default App
---
+++ src/components/model/PerformanceModel.ts
@@ -0,0 +1,7 @@
+export interface  LocalizedModel {
+    ar: string;
+    en: string;
+}
+export interface PerformanceModel {
+    name: LocalizedModel;
+}
