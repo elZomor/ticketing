d2946032f3b1ed527d9617b195333c9c848582a9
Mohamed Elzomor <mohamed.elzomor@outlook.com>
2024-09-16 02:08:58 +0300

    fix(EGT-6): add localization for project

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ClerkProvider } from '@clerk/clerk-react';
+import './assets/localization/i18n.js';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -15,5 +16,5 @@ createRoot(document.getElementById('root')!).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
     </ClerkProvider>
-  </StrictMode>
+  </StrictMode>,
 );
