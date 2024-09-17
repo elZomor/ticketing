f1f06345f4e33e17991cec4c1bc219444e1ac3d0
Mohamed Elzomor <mohamed.elzomor@outlook.com>
2024-09-18 02:27:19 +0300

    feat(EGT-22): add routes components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/header/Header.tsx';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/about/About.tsx';
+import NotFound from './components/notFoundPage/NotFound.tsx';
+import ShowsViewAll from './components/shows/ShowsViewAll.tsx';
+import TheaterScriptsViewAll from './components/theaterScripts/TheaterScriptsViewAll.tsx';
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/theater-scripts" element={<TheaterScriptsViewAll />} />
+        <Route path="/shows" element={<ShowsViewAll />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
