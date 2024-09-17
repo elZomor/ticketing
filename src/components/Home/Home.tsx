fef61b82b27414392ad4577ea6a85dcaf5ca4c5c
Mohamed Elzomor <mohamed.elzomor@outlook.com>
2024-09-17 03:08:41 +0300

    feat(EGT-15): add navBar and persist localization update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,20 +1,10 @@
 import seats12 from '../../assets/images/seats12.avif';
-import { useClerk, SignedOut, SignedIn } from '@clerk/clerk-react';
 import { useTranslation } from 'react-i18next';
+import { SignedOut, useClerk } from '@clerk/clerk-react';
 
 export default function Home() {
+  const { t } = useTranslation();
   const clerk = useClerk();
-  const { t, i18n } = useTranslation();
-  const handleLogin = () => {
-    clerk.openSignIn();
-  };
-  const handleLogout = async () => {
-    await clerk.signOut();
-  };
-  const handleLanguageChange = async () => {
-    const currentLanguage = i18n.language;
-    await i18n.changeLanguage(currentLanguage === 'en' ? 'ar' : 'en');
-  };
 
   return (
     <div className="w-screen h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
@@ -29,28 +19,14 @@ export default function Home() {
           </p>
           <SignedOut>
             <button
-              onClick={handleLogin}
+              onClick={() => clerk.openSignIn()}
               className="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10"
             >
               {t('LOGIN')}
             </button>
           </SignedOut>
-          <SignedIn>
-            <button
-              onClick={handleLogout}
-              className="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10"
-            >
-              {t('LOGOUT')}
-            </button>
-          </SignedIn>
           <button className="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10 mx-3">
             {t('SHOWS')}
-          </button>
-          <button
-            onClick={handleLanguageChange}
-            className="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10 mx-3"
-          >
-            {t('OTHER_LANGUAGE')}
           </button>
         </div>
       </div>
