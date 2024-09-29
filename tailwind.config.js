2da26b8f72dc7e1a7f959d8ce5eae82a434793b3
zomor <mohamed.elzomor@outlook.com>
2024-09-29 21:58:10 +0300

    feat(EGT-53): add colors to the tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#8B0000',
+        secondaryColor: '#E17055',
+        accentColor: '#F9A825',
+        neutralColorOne: '#F5F5F5',
+        neutralColorTwo: '#FAF3E0',
+        textColorMain: '#333333',
+        textColorAlt: '#3E2723',
+        darkRed: '#531812',
+        gold: '#db9241',
+      },
+    },
   },
   plugins: [],
 };
