297ffc9324861f3fb748de202ffac11da01fe495
zomor <mohamed.elzomor@outlook.com>
2024-09-30 21:27:54 +0300

    feat(EGT-28): update hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -3,26 +3,38 @@ import hero from '../../assets/images/hero1.jpg';
 import { Link } from 'react-router-dom';
 
 export const HeroSection = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center h-full">
       <div
-        className="flex flex-col-reverse md:flex-row px-6 md:px-20 md:w-[90%] bg-cover bg-center bg-no-repeat
-     mt-4 p-6 items-center justify-between md:h-80 bg-accentColor
+        className="flex flex-col md:flex-row px-6 md:px-10 w-full bg-cover bg-center bg-no-repeat
+      p-6 items-center justify-between md:h-[25rem] bg-accentColor
      bg-opacity-70 shadow-xl"
         style={{ backgroundImage: `url(${hero})` }}
       >
-        <div className="h-full flex flex-col items-center justify-center">
-          <h2 className="text-4xl italic lg:text-5xl text-center md:text-right text-neutralColorOne leading-tight font-bold">
+        <div className="h-full flex flex-col items-center justify-center md:w-1/2">
+          <h2
+            className={`text-4xl lg:text-5xl ${i18n.language === 'ar' ? 'md:text-right' : 'md:text-left'} text-neutralColorOne font-bold w-full`}
+          >
             {t('HERO_TITLE')}
           </h2>
-          <h3 className="mt-6 md:mt-10 text-md lg:text-xl text-center md:text-right text-neutralColorTwo font-light tracking-wider leading-relaxed">
-            {t('HERO_SECTION')}
+          <h3
+            className={`mt-6 md:mt-10 text-md lg:text-xl ${i18n.language === 'ar' ? 'md:text-right' : 'md:text-left'}
+          text-neutralColorTwo font-light tracking-wider leading-relaxed`}
+          >
+            {t('HERO_SECTION')} {t('HERO_SECTION')} {t('HERO_SECTION')}{' '}
+            {t('HERO_SECTION')} {t('HERO_SECTION')} {t('HERO_SECTION')}{' '}
+            {t('HERO_SECTION')} {t('HERO_SECTION')}
           </h3>
+        </div>
+        <div
+          className={`md:absolute bottom-4  ${i18n.language === 'en' ? 'md:right-4' : 'md:left-4'}`}
+        >
           <Link
             to="/shows"
-            className="mt-10 inline-block bg-textColorMain text-neutralColorOne font-semibold px-6 py-3
-            rounded-md hover:bg-textColorAlt transition-colors duration-300"
+            className="mt-3 inline-block bg-textColorMain text-neutralColorOne font-semibold px-6 py-3 md:px-10 md:py-5
+                        rounded-md hover:bg-textColorAlt transition-colors duration-300 md:text-3xl
+                         "
           >
             {t('DETAILS')}
           </Link>
